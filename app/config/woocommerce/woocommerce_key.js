8839256a06abd9ebcadb486e9d324cfd45f212bc
Azharul Haque <phpgladiator@gmail.com>
2021-06-01 19:01:10 +0600

    categorylist v1 done

---
+++ app/config/woocommerce/woocommerce_key.js
@@ -0,0 +1,13 @@
+// export const BASE_URL = "http://www.woospider.com/wp-json/wc/v3/";
+// export const CONSUMER_KEY = "ck_d0a8eb55557b8095526229c79944f1a2551e2fe2";
+// export const CONSUMER_SECRET = "cs_f299acfaee2f87434ef941415f0770a727586e20";
+
+//LOCAl
+// export const BASE_URL = "https://81a46e4858f8.ngrok.io/wp-json/wc/v3/";
+// export const CONSUMER_KEY = "ck_e682f5cdb5dd3b2cd55bcbcde340fca5ca11a2a8";
+// export const CONSUMER_SECRET = "cs_61e395247f61403c3b7344416348dd2874cbfd69";
+
+//Ovedh.com
+export const BASE_URL = "https://www.ovedh.com/wp-json/wc/v3/";
+export const CONSUMER_KEY = "ck_04fc33fb47fefae1de286c1a7ce0bf32a56b64bb";
+export const CONSUMER_SECRET = "cs_1b1ec2c83c5d100f6aaeb051cd9c0eb0d0013259";
